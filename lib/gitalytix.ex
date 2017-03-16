89b210a6bb96522a4ca0aa5c6a5b556555012407
Gonzalo Robaina <gonzalor@gmail.com>
2017-03-15 22:56:46 -0300

    Initial commit

---
+++ lib/gitalytix.ex
@@ -0,0 +1,6 @@
+defmodule Gitalytix do
+
+  def hello do
+    :world
+  end
+end
---
+++ test/gitalytix_test.exs
@@ -0,0 +1,8 @@
+defmodule GitalytixTest do
+  use ExUnit.Case
+  doctest Gitalytix
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
