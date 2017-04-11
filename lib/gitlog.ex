1382247bd7d114965864b6536e3db5cbbab26ad5
Gonzalo Robaina <gonzalor@gmail.com>
2017-04-11 04:19:42 -0300

    Basic command line report now working

--- lib/gitlog.ex
+++ lib/gitlog.ex
@@ -2,24 +2,62 @@ defmodule GitLog do
   require IEx
 
   def main do
-    get_log() |> get_blocks |> parse_git_log
+    get_log() |> get_commits |> parse_git_log
   end
 
-  defp parse_git_log(blocks) do
-    IEx.pry
+  defp parse_git_log(commits) do
+    authors = Enum.map(commits, fn x -> %{ name: Map.get(x, "name") } end) |> Enum.dedup
+    authors = parse_authors(commits, authors, [])
+
+    print_summary(authors)
   end
 
   defp get_log do
-    log = System.cmd "git", ["log", "--numstat", "--pretty='%H %ai %an <%ae> %s'"]
+    log = System.cmd "git", ["log", "--numstat", "--pretty=\#%H %aI %an <%ae> %s .::."]
     elem(log, 0)
   end
 
-  defp get_blocks(log_string) do
-    commits = Regex.scan(~r/([a-f0-9]{40})/, log_string)
-    commits = Enum.map(commits, fn x -> List.first(x) end)
-    blocks  = Regex.split(~r/[a-f0-9]{40}/, log_string)
-    blocks  = Enum.map(blocks, fn x -> String.strip(x) end)
-    blocks  = Enum.drop(blocks, 1) # Get rid of the first empty string
-    Enum.zip(commits, blocks)
+  defp get_commits(log_string) do
+    regex = ~r/^(?<hash>[a-f0-9]{40}) (?<date>.{25}) (?<name>.*?) \<(?<email>.*?)\> (?<subject>.*?) .::.(?<summary>.*)$/mus
+    Regex.split(~r/(?<tag>#)[a-f0-9]{40}/, log_string, on: [:tag], trim: true)
+    |> Enum.map(fn x -> String.strip(x) end)
+    |> Enum.map(fn x -> Regex.named_captures(regex, x) end)
+  end
+
+  defp get_author_commits(commits, author) do
+    commits |> Enum.filter(fn x -> Map.get(x, "name") == author end)
+  end
+
+  defp gregorian_day_for_datetime(datetime) do
+    DateTime.to_date(datetime) |> Date.to_erl |> :calendar.date_to_gregorian_days
+  end
+
+  defp parse_authors(commits, [author | tail], result) do
+    author_commits = get_author_commits(commits, Map.get(author, :name))
+    commits_dates = author_commits |> Enum.map(&(Map.get(&1, "date"))) |> Enum.map(&(elem(DateTime.from_iso8601(&1), 1)))
+
+    author = Map.put(author, :count, Enum.count(author_commits))
+    author = Map.put(author, :first_commit, Enum.min(commits_dates))
+    author = Map.put(author, :last_commit, Enum.max(commits_dates))
+
+    commits_dates_diff = gregorian_day_for_datetime(author[:last_commit]) - gregorian_day_for_datetime(author[:first_commit])
+    author = Map.put(author, :date_span, commits_dates_diff)
+    author = Map.put(author, :working_days, Enum.count(Enum.dedup(Enum.map(commits_dates, fn x -> DateTime.to_date(x) end))))
+
+    result = [author | result]
+    parse_authors(commits, tail, result)
+  end
+
+  defp parse_authors(_commits, [], result) do
+    result
+  end
+
+  defp print_summary([author | tail]) do
+    IO.puts "#{Map.get(author, :name)} has made #{Map.get(author, :count)} commits on #{Map.get(author, :working_days)} separate days during a span of #{Map.get(author, :date_span)} days."
+    print_summary(tail)
+  end
+
+  defp print_summary([]) do
+
   end
 end
